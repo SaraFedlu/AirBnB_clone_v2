0cc044fe60068c4d59b19551b4aabe94f29b113d
Sara Fedlu <sfedlu28@gmail.com>
2024-05-21 00:30:58 -0700

    pass pep8 validation

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,13 +4,15 @@ from fabric.api import local
 from datetime import datetime
 import os
 
+
 def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder.
 
     The archive will be stored in a folder named 'versions', which will be
     created if it does not exist. The name of the archive will include the
-    current timestamp in the format web_static_<year><month><day><hour><minute><second>.tgz.
+    current timestamp in the format web_static_<year><month><day><hour>\
+    <minute><second>.tgz.
 
     Returns:
         str: The path to the created archive if successful, otherwise None.
@@ -18,17 +20,17 @@ def do_pack():
     # Create the versions directory if it doesn't exist
     if not os.path.exists("versions"):
         os.makedirs("versions")
-    
+
     # Generate the archive name with the current date and time
     now = datetime.now()
-    archive_name = "versions/web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
-    
+    archive_name = "versions/web_static_{}.tgz".format(now.strftime(
+        "%Y%m%d%H%M%S"))
+
     # Create the archive from the web_static folder
     result = local("tar -czvf {} web_static".format(archive_name))
-    
+
     # Check if the archive was created successfully and return the path or None
     if result.succeeded:
         return archive_name
     else:
         return None
-
